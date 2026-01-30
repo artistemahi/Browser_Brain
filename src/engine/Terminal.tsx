eef932663257a3922aa9d973419fecb95b84f3d4
Mahesh kumar <vickymandal542@gmail.com>
2026-01-31 03:44:02 +0530

    dot animate add

--- src/engine/Terminal.tsx
+++ src/engine/Terminal.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const Terminal = () => {
   return (
     <div className="bg-black text-green-500 font-mono p-4 mt-8 rounded-lg w-96 h-48 flex ">
-      <span className="text-red-500 text-2xl">&#9679;</span>
-      <span className="text-yellow-500 text-2xl">&#9679;</span>
-      <span className="text-green-500 text-2xl">&#9679; </span>
+      <span className="text-red-500 text-2xl animate-pulse [animation-delay:0ms]">&#9679;</span>
+      <span className="text-yellow-500 text-2xl animate-pulse [animation-delay:200ms]">&#9679;</span>
+      <span className="text-green-500 text-2xl animate-pulse [animation-delay:400ms]">&#9679; </span>
       <p className="mt-1.5 ml-4">Terminal Content</p>
     </div>
   );
