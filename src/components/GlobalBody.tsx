f72abb5211cbdf24c276e4460d88f6249d7e85e9
Mahesh kumar <vickymandal542@gmail.com>
2026-02-05 19:37:31 +0530

    gsap and updated the favicon

--- src/components/GlobalBody.tsx
+++ src/components/GlobalBody.tsx
@@ -1,24 +1,23 @@
-import { PlayIcon, BackwardStep, ForwardStep } from "../../utils/constants";
+import { PlayIcon, BackwardStep, ForwardStep,StopIcon } from "../../utils/constants";
 import FooterPage from "../pages/FooterPage";
 const GlobalBody = () => {
   return (
     <div className="bg-gray-300 min-h-dvh ">
       {/* heading section */}
       <h1 className="bg-amber-300 py-4 text-2xl font-bold text-gray-900 text-center">
-        some moving text
+        some moving text/animations
       </h1>
       {/* body section */}
       <div className="bg-green-400 flex justify-evenly min-h-dvh ">
         {/* left pannel */}
         <div className="flex flex-col space-y-8 text-white border-green-400 border bg-gray-950 p-4 w-48 ">
-          <p>this is left pannel</p>
           <ul> Event Loop</ul>
           <ul> Async Lab</ul>
           <ul> Rendering</ul>
           <ul> Performance</ul>
         </div>
         {/* main body */}
-        <div className="flex-1 bg-fuchsia-50 px-10">this is Main Body</div>
+        <div className="flex-1 bg-fuchsia-50 px-10 text-center">this is Main Body</div>
         {/* right pannel */}
         <div className="bg-gray-950  border-green-400 border p-4 text-white  ">
          <div className="flex flex-col space-y-8  ">
@@ -27,10 +26,17 @@ const GlobalBody = () => {
             <span> Run</span>
           </button>
           <button>
+            <StopIcon />
+            <span> Stop</span>
+          </button>
+          <button>
             <BackwardStep /> Back
           </button>
           <button>
             <ForwardStep /> Next
+          </button>
+          <button className="ml-4">
+              Why this?
           </button>
           </div>
         </div>
--- utils/constants.tsx
+++ utils/constants.tsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay, faStepBackward, faStepForward } from "@fortawesome/free-solid-svg-icons";
-import React from "react";
+import { faPause, faPlay, faStepBackward, faStepForward } from "@fortawesome/free-solid-svg-icons";
+import React from "react"; // this line important to show this icons 
 
 export const PlayIcon = () => (
   <FontAwesomeIcon icon={faPlay} bounce size="sm" style={{ color: "#63E6BE" }} />
@@ -16,6 +16,14 @@ export const BackwardStep = () => (
 export const ForwardStep = () => (
   <FontAwesomeIcon
     icon={faStepForward}
+    beatFade
+    size="sm"
+    style={{ color: "#63E6BE" }}
+  />
+);
+export const StopIcon = () => (
+  <FontAwesomeIcon
+    icon={faPause}
     beatFade
     size="sm"
     style={{ color: "#63E6BE" }}
