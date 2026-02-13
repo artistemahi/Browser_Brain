938aac3470bfa943b1fabe706995d9e722d42dfe
Mahesh kumar <vickymandal542@gmail.com>
2026-02-13 19:47:34 +0530

    updated the renderingbody

--- src/components/RenderingBody.tsx
+++ src/components/RenderingBody.tsx
@@ -1,48 +1,69 @@
-import FooterPage from '../pages/FooterPage'
-import  { Link } from 'react-router-dom'
-import { PlayIcon, StopIcon, BackwardStep, ForwardStep } from '../../utils/constants'
-import RenderingContent from './RenderingContent'
+import FooterPage from "../pages/FooterPage";
+import { Link } from "react-router-dom";
+import {
+  PlayIcon,
+  StopIcon,
+  BackwardStep,
+  ForwardStep,
+} from "../../utils/constants";
+import RenderingContent from "./RenderingContent";
 
 const RenderingBody = () => {
   return (
-    <div><div className="bg-gray-300 min-h-dvh ">
-      {/* body section */}
-      <div className="bg-green-400 flex justify-evenly min-h-dvh ">
-        {/* left pannel */}
-        <div className="flex flex-col space-y-8 text-white border-green-400 border bg-gray-950 p-4 w-48 ">
-          <Link to="/rendering"> <button> Rendering</button></Link>
-        </div>
-        {/* main body */}
-        <div className="flex-1 bg-fuchsia-50 px-10 text-center"><RenderingContent /></div>
-        {/* right pannel */}
-        <div className="bg-[rgb(28,33,48)] border-green-400 border p-4 text-white  ">
-         <div className="flex flex-col space-y-8  ">
-          <button>
-            <PlayIcon />
-            <span> Run</span>
-          </button>
-          <button>
-            <StopIcon />
-            <span> Stop</span>
-          </button>
-          <button>
-            <BackwardStep /> Back
-          </button>
-          <button>
-            <ForwardStep /> Next
-          </button>
-          <button className="ml-4">
-              Why this?
-          </button>
+    <div>
+      <div className="bg-gray-300 min-h-dvh ">
+        {/* body section */}
+        <div className="bg-green-400 flex justify-evenly min-h-dvh ">
+          {/* left pannel */}
+          <div className="flex flex-col space-y-8 text-white border-green-400 border bg-gray-950 p-4 w-48 ">
+            <Link to="/rendering">
+              {" "}
+              <button> Rendering</button>
+            </Link>
+            <section>
+              <ul>About Rendering</ul>
+              <ul>Types</ul>
+            </section>
+            <section>
+              <ul>Dom</ul>
+              <ul>Style</ul>
+              <ul>Layout</ul>
+              <ul>Pain</ul>
+              <ul>Composite</ul>
+            </section>
+          </div>
+          {/* main body */}
+          <div className="flex-1 bg-fuchsia-50 px-10 text-center">
+            <RenderingContent />
+          </div>
+          {/* right pannel */}
+          <div className="bg-[rgb(28,33,48)] border-green-400 border p-4 text-white  ">
+            <div className="flex flex-col space-y-8  ">
+              <button>
+                <PlayIcon />
+                <span> Run</span>
+              </button>
+              <button>
+                <StopIcon />
+                <span> Stop</span>
+              </button>
+              <button>
+                <BackwardStep /> Back
+              </button>
+              <button>
+                <ForwardStep /> Next
+              </button>
+              <button className="ml-4">Why this?</button>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div>
-        <FooterPage />
+        <div>
+          <FooterPage />
+        </div>
       </div>
-    </div></div>
-  )
-}
+    </div>
+  );
+};
 
-export default RenderingBody
+export default RenderingBody;
--- src/components/RenderingContent.tsx
+++ src/components/RenderingContent.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import StickyHeadTable from "./../../utils/renderingTable";
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
