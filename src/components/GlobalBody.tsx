ef1e8e2a3e55a76dc16ed4a376fd78c63e01ac10
Mahesh kumar <vickymandal542@gmail.com>
2026-02-25 01:29:15 +0530

    scroll effect

--- src/components/GlobalBody.tsx
+++ src/components/GlobalBody.tsx
@@ -4,6 +4,7 @@ import {
   ForwardStep,
   StopIcon,
 } from "../../utils/constants";
+import MainContent from "./MainContent";
 import FooterPage from "../pages/FooterPage";
 import Reordering from "./../../utils/Reordering";
 import Heroanimation from "../../utils/heroanimation";
@@ -20,7 +21,7 @@ const GlobalBody = () => {
       {/* body section */}
       <div className="bg-green-400 flex justify-evenly min-h-dvh ">
         {/* left pannel */}
-        <div className="flex flex-col space-y-8 text-white border-green-400 border bg-gray-950 p-4 w-48 ">
+        <div className="flex flex-col space-y-8 text-white border-green-400 border bg-neutral-950 p-4 w-48 ">
           <Link to="/rendering">
             {" "}
             <button> Rendering</button>
@@ -35,8 +36,8 @@ const GlobalBody = () => {
           </Link>
         </div>
         {/* main body */}
-        <div className="flex-1 bg-fuchsia-50 px-10 text-center">
-          main content
+        <div className="flex-1 bg-neutral-800 px-10 text-center text-white ">
+          <MainContent />
         </div>
         {/* right pannel */}
         <div className="bg-[rgb(28,33,48)] border-green-400 border p-4 text-white  ">
---
+++ src/components/MainContent.tsx
@@ -0,0 +1,9 @@
+import ScrollEffect from "../engine/ScrollEffect";
+const MainContent =() => {
+return (
+    <div >
+        <ScrollEffect />
+    </div>
+)
+}
+export default MainContent;
