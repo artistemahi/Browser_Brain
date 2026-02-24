fbd89c49a947fd6f1921899a178fde464b2c324a
Mahesh kumar <vickymandal542@gmail.com>
2026-02-25 00:38:44 +0530

    footer/updated

--- src/pages/FooterPage.tsx
+++ src/pages/FooterPage.tsx
@@ -1,7 +1,11 @@
+import logoVideo from "../assets/logo/Juliana.mp4";
 const  FooterPage = () => {
   return (
-    <div className="bg-gray-800 text-white py-4 text-center">
-      <p>&copy; 2026 Your Company. All rights reserved.</p>
+    <div className="bg-black text-white  text-center flex justify-center items-center flex-col space-y-2">
+      
+        <video src={logoVideo} autoPlay muted loop className=" mt-14 h-30 w-auto " ></video>
+      
+      <p>&copy; 2026 Browser Brain. All rights reserved.</p>
       <p>
         <a href="/privacy" className="text-gray-400 hover:text-gray-200">
           Privacy Policy
